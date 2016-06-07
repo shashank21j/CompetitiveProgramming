cd436315c9bba6c35070838a57484c5b10330f37
shashank21j <shashank@hackerrank.com>
2016-06-07 14:19:03 +0530

    update

--- Uva/1_Introduction/GettingStarted/SuperEasy/272.cpp
+++ Uva/1_Introduction/GettingStarted/SuperEasy/272.cpp
@@ -2,23 +2,28 @@
 
 using namespace std;
 
-int main() {
-  string st;
-  int flag;
-  flag = 0;
-  while( getline(cin,st) ) {
-    for (int i=0; i< st.length(); i++) {
-      if (st[i] == '"' && flag == 0) {
-        printf("``");
-        flag = 1;
-      }
-      else if (st[i] == '"' && flag == 1) {
-	printf("''");
-        flag = 0;
-      }    
-      else printf("%c",st[i]);
+int main () {
+    string s;
+    int flag = 0;
+    while(getline(cin,s)) {
+        string out;
+        out = "";
+        for (int i = 0;i<s.length();i++) {
+            if (s[i] == '"') {
+                if (flag == 0) {
+                    out += "``";
+                }
+                else {
+                    out += "''";
+                }
+                flag += 1;
+                flag %= 2;
+            }
+            else {
+                out += s[i];
+            }
+        }
+        cout<<out<<endl;
     }
-    cout <<"\n";
-  }
-  return 0;
+    return 0;
 }
