7dbfea013195f79f59aef6c9e143276667cd88ed
shashank21j <shashank@hackerrank.com>
2016-07-17 11:45:22 +0530

    new

---
+++ Uva/1_Introduction/AdHocPart1/InterestingRealLifeProblemsEasier/12555.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Warning common logic is defied. The ss>symb has read spaces too :-o
+*/
+stringstream ss;
+int main() {
+    int t;
+    string inp;
+    cin>> t;
+    int l1;
+    int l2;
+    string symb;
+    getline(cin, inp);
+    int idx = 0;
+    while (t--) {
+        getline(cin, inp);
+        ss.str("");
+        ss.clear();
+        ss.str(inp);
+        ss >> l1;
+        ss >> symb;
+        if (symb.length() > 3) {
+            l2 = symb[3] - 48;
+        }
+        else {
+            l2 = 0;
+        }
+        cout<<"Case "<<++idx<<": "<<l1*0.5 + l2*0.05<<endl;
+    }
+    return 0;
+}
