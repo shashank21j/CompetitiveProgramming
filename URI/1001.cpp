b3ebe9db3c71411ebdf91970bb6f79fd3486093c
shashank21j <shashank@hackerrank.com>
2016-08-21 18:59:43 +0530

    :)

--- URI/1001.cpp
+++ URI/1001.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h> 
- 
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
-int a; int b;
-cin>>a>>b;
-cout<<"X = "<<a+b<<endl;
- return 0;
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << "X = " << a + b << endl;
+    return 0;
 }
--- URI/1002.cpp
+++ URI/1002.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
 	std::setlocale(LC_ALL, "en_US.UTF-8");
     std::setlocale(LC_NUMERIC, "en_US");
 	double R;
-	cin>>R;
-	printf("A=%.4lf\n",3.14159*(R*R));
+	cin >> R;
+	printf("A=%.4lf\n", 3.14159 * (R * R));
 	return 0;
 }
